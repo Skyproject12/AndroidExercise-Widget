b90e95fc8f4a33d48d78280657e8882ae26248c3
Skyproject12 <sumendrakadek108@gmail.com>
2019-10-06 16:23:02 +0700

    menambahkan button yang saaat diklik akan memberi broadcash yang merequest suatu receiver, di dalam receiver akan mengubah text menjadi bialangan random

--- app/src/main/java/com/example/widgethomescreen/RandomNumberWidget.java
+++ app/src/main/java/com/example/widgethomescreen/RandomNumberWidget.java
@@ -1,11 +1,15 @@
 package com.example.widgethomescreen;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class RandomNumberWidget extends AppWidgetProvider {
+
+    private static String WIDGET_CLICK = "widgetsclick";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -15,10 +19,11 @@ public class RandomNumberWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_number_widget);
 
         // class Number Generator with generate 100
-        String lastUpdate= "Random" + NumberGenerator.Generate(100);
+        String lastUpdate = "Random" + NumberGenerator.Generate(100);
         // call function random dan set fungsi tersebut kedalam widget dengan id appwidget_text
         views.setTextViewText(R.id.appwidget_text, lastUpdate);
-
+        // set onclick button  with function pending intent
+        views.setOnClickPendingIntent(R.id.button_click, getPendingSelfIntent(context, appWidgetId, WIDGET_CLICK)); // mengeset action berupan WIDGET_CLICK agar dienali pada receiver
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -40,5 +45,36 @@ public class RandomNumberWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
+
+    // sebagai media penerima broadcahc yang di kirim dari method getPendingSelfIntent
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        // ketika WIDGET CLICK equals intent getAction
+        if (WIDGET_CLICK.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_number_widget);
+            String lastUpdate = "Random" + NumberGenerator.Generate(100);
+            // mengambil kiriman dari getPendingSelfIntent
+            int appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0);
+            views.setTextViewText(R.id.appwidget_text, lastUpdate);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    private static String WIDGET_ID_EXTRA = "widget_id_extra";
+     static PendingIntent getPendingSelfIntent(Context context, int appWidgetId, String action){
+         //intent to random number widget
+        Intent intent= new Intent(context, RandomNumberWidget.class);
+        // menambahkan inetnt put extra untuk mengidentifikasi widget yang akan dieksekusi
+        intent.setAction(action);
+        intent.putExtra(WIDGET_ID_EXTRA, appWidgetId);
+        // ketika melakukan suatu broadcach tentu harus ada receivernya
+        return PendingIntent.getBroadcast(context, appWidgetId, intent, 0);
+    }
+}
+// jadi alurnya
+//lakukan click buttom maka akan mengeksekusi method getPendingSelftIntent
+// method tersebut akan memberi retur berupa broadcahs
+// nantinya akan diterima pada receiver dan mengubah dari textview berdasarkan variable lastUpdate
